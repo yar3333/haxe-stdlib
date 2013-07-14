1bb575b8f4ca8b3131234cdf31959758b1324cc9
Yaroslav Sivakov <yar3333@gmail.com>
2013-07-15 01:13:08 +0400

    * simplificate exception class

--- library/stdlib/Exception.hx
+++ library/stdlib/Exception.hx
@@ -6,61 +6,18 @@ using StringTools;
 class Exception 
 {
 	public var message(default, null) : String;
-	public var stackTrace(default, null) : Array<StackItem>;
-	public var innerException(default, null) : Exception;
-	public var baseException(get_baseException, null) : Exception;
+	public var stack(default, null) : Array<StackItem>;
 
-	public function new(?message:String, ?innerException:Dynamic)
+	public function new(?message:String)
 	{
 		this.message = message == null ? "" : message;
-		
-		if (innerException != null)
-		{
-			if (Std.is(innerException, Exception))
-			{
-				this.innerException = innerException;
-			}
-			else
-			{
-				this.innerException = new Exception(Std.string(innerException), null);
-				this.innerException.stackTrace = Stack.exceptionStack();
-			}
-		}
-		
-		stackTrace = Stack.callStack();
-		stackTrace.shift();
+		stack = Stack.callStack();
+		stack.shift();
 	}
 
-	function get_baseException() 
-	{
-		var inner = this;
-		while (inner.innerException != null)
-		{
-			inner = inner.innerException;
-		}
-		return inner;
-	}
-	
 	public function toString() : String
 	{
-		var innerString = innerException != null ? innerException.toString() : "";
-		return message 
-			+ "\n\tStack trace:" + Stack.toString(stackTrace).replace("\n", "\n\t\t") 
-			+ (innerString != "" ? "\nINNER EXCEPTION: " + innerString : "");
-	}
-	
-	public static function toText(exception:Dynamic) : String
-	{
-		var text = "EXCEPTION: ";
-		if (Std.is(exception, Exception))
-		{
-			text += exception.toString();
-		}
-		else
-		{
-			text += Std.string(exception) + "\n\tStack trace:" + Stack.toString(Stack.exceptionStack()).replace("\n", "\n\t\t");
-		}
-		return text;
+		return message + "\nStack trace:\n" + Stack.toString(stack).replace("\n", "\n\t");
 	}
 	
 	public static function rethrow(exception:Dynamic) : Void
@@ -72,12 +29,12 @@ class Exception
 		#end
 	}
 	
-	public static function wrap(exception:Dynamic) : Exception
+	static function wrap(exception:Dynamic) : Exception
 	{
 		if (!Std.is(exception, Exception))
 		{
 			var r = new Exception(Std.string(exception));
-			r.stackTrace = haxe.Stack.exceptionStack();
+			r.stack = Stack.exceptionStack();
 			return r;
 		}
 		return exception;
