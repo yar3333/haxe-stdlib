1a430f74bd967129741a722a3a838dfb8d473c73
Yaroslav Sivakov <yar3333@gmail.com>
2013-11-15 09:20:30 +0400

    + serializer / unserializer

---
+++ library/stdlib/Unserializer.hx
@@ -0,0 +1,11 @@
+package stdlib;
+
+class Unserializer
+{
+	public static function run(v:String, ?r:haxe.Unserializer.TypeResolver) : Dynamic
+	{
+		var unserializer = new haxe.Unserializer(v);
+		if (r != null) unserializer.setResolver(r);
+		return unserializer.unserialize();
+	}
+}
