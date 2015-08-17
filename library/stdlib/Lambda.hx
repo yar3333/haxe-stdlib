21a98fca8c6b74eda4d1ebfa64ff9198ff9b4cff
Yaroslav Sivakov <yar3333@gmail.com>
2015-08-17 20:13:07 +0300

    * Std.array() -> Lambda.array()

--- library/stdlib/Lambda.hx
+++ library/stdlib/Lambda.hx
@@ -2,19 +2,8 @@ package stdlib;
 
 typedef Lambda = std.Lambda;
 
-class LambdaEx
+class LambdaArray
 {
-	public static function findIndex<A>(it:Iterable<A>, f:A->Bool) : Int
-	{
-		var n = 0;
-		for (x in it)
-		{
-			if (f(x)) return n;
-			n++;
-		}
-		return -1;
-	}
-   
 	public static function insertRange<A>(arr:Array<A>, pos:Int, range:Array<A>) : Void
 	{
 		for (e in range)
@@ -38,6 +27,77 @@ class LambdaEx
 				i++;
 			}
 		}
+		return r;
+	}
+}
+
+class LambdaIterable
+{
+	public static function findIndex<A>(it:Iterable<A>, f:A->Bool) : Int
+	{
+		var n = 0;
+		for (x in it)
+		{
+			if (f(x)) return n;
+			n++;
+		}
+		return -1;
+	}
+	
+	public static function sorted<A>(it:Iterable<A>, ?cmp:A->A->Int) : Array<A>
+	{
+		var r = Lambda.array(it);
+		r.sort(cmp != null ? cmp : Reflect.compare);
+		return r;
+	}
+}
+
+class LambdaIterator
+{
+	public static function array<A>(it:Iterator<A>) : Array<A>
+	{
+		var r = new Array<A>();
+		for (e in it) r.push(e);
+		return r;
+	}
+	
+	public static function map<A,R>(it:Iterator<A>, conv:A->R) : Array<R>
+	{
+		var r = new Array<R>();
+		for (e in it) r.push(conv(e));
+		return r;
+	}
+	
+	public static function filter<A>(it:Iterator<A>, pred:A->Bool) : Array<A>
+	{
+		var r = new Array<A>();
+		for (e in it) if (pred(e)) r.push(e);
+		return r;
+	}
+	
+	public static function count<A>(it:Iterator<A>, ?pred:A->Bool)
+	{
+		var n = 0;
+		if(pred == null) for (_ in it) n++;
+		else             for (x in it) if (pred(x)) n++;
+		return n;
+	}
+	
+	public static function findIndex<A>(it:Iterator<A>, f:A->Bool) : Int
+	{
+		var n = 0;
+		for (x in it)
+		{
+			if (f(x)) return n;
+			n++;
+		}
+		return -1;
+	}
+	
+	public static function sorted<A>(it:Iterator<A>, ?cmp:A->A->Int) : Array<A>
+	{
+		var r = array(it);
+		r.sort(cmp != null ? cmp : Reflect.compare);
 		return r;
 	}
 }
