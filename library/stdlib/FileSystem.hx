2134f5f2bf2acbe6094f068a123566433bab2a28
Yaroslav Sivakov <yar3333@gmail.com>
2015-09-25 14:24:47 +0300

    * refactor FileSystem

--- library/stdlib/FileSystem.hx
+++ library/stdlib/FileSystem.hx
@@ -1,73 +1,12 @@
 package stdlib;
 
+#if !macro
+
+@:build(stdlib.Macro.forwardStaticMethods(sys.FileSystem))
 class FileSystem 
 {
 	/**
-	*	Tells if the given file or directory exists.
-	*/
-	public static inline function exists(path:String) : Bool return sys.FileSystem.exists(path);
-
-	/**
-		Rename the corresponding file or directory, allow to move it accross directories as well.
-	*/
-	public static inline function rename(path:String, newpath:String) : Void sys.FileSystem.rename(path, newpath);
-
-	/**
-		Returns informations for the given file/directory.
-	*/
-	public static inline function stat(path:String) : sys.FileStat return sys.FileSystem.stat(path);
-
-	/**
-		Returns the full path for the given path which is relative to the current working directory.
-	*/
-	public static inline function fullPath(relpath:String) : String return sys.FileSystem.fullPath(relpath);
-
-	/**
-		Tells if the given path is a directory. Throw an exception if it does not exists or is not accesible.
-	*/
-	public static inline function isDirectory(path:String) : Bool return sys.FileSystem.isDirectory(path);
-
-	/**
-		Create the given directory. Recursive: the parent directory will be created too.
-	*/
-	public static function createDirectory(path:String) : Void
-	{
-		if (!exists(path))
-		{
-			path = StringTools.replace(path, '\\', '/');
-			
-			if (path.substr(-1) == "/")
-			{
-				path = path.substr(0, path.length - 1);
-			}
-			
-			var dirs = path.split('/');
-			for (i in 0...dirs.length)
-			{
-				var dir = dirs.slice(0, i + 1).join('/');
-				if (dir != "" && dir.substr(-1) != ':')
-				{
-					try
-					{
-						if (!exists(dir))
-						{
-							sys.FileSystem.createDirectory(dir);
-						}
-					}
-					catch (e:Dynamic)
-					{
-						if (i == dirs.length - 1)
-						{
-							Exception.rethrow(e);
-						}
-					}
-				}
-			}
-		}
-	}
-
-	/**
-		Delete a given file.
+		Delete a given file if it is exists.
 	*/
 	public static function deleteFile(path:String) : Void
 	{
@@ -78,7 +17,7 @@ class FileSystem
 	}
 	
 	/**
-		Delete a given directory.
+		Delete a given directory recursively.
 	*/
 	public static function deleteDirectory(path:String) : Void
 	{
@@ -99,9 +38,10 @@ class FileSystem
 			sys.FileSystem.deleteDirectory(path);
 		}
 	}
-
-	/**
-		Read all the files/directories stored into the given directory.
-	*/
-	public static inline function readDirectory(path:String) : Array<String> return sys.FileSystem.readDirectory(path);
 }
+
+#else
+
+typedef FileSystem = sys.FileSystem;
+
+#end
